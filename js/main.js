4232d15a2b41d3bd771679d383c46c7aa79e8082
Sam Cochrane <samcochrane@Sams-MacBook-Air.local>
2018-03-05 19:53:29 +1100

    unit 8

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log('User flipped', cardOne)
-console.log('User flipped', cardThree)
-console.log('User flipped', cardOnlog
+//alert ('Hello friends');
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay =[];
+
+const cardOne = cards[0]
+const cardTwo = cards[2]
+
+
+cardsInPlay.push (cardOne)
+cardsInPlay.push (cardTwo)
+
+
+console.log(`user flipped ${cardOne}`);
+console.log(`user flipped ${cardTwo}`);
+
+if (cardsInPlay.length === 2){
+	if (cardOne === cardTwo) {
+alert('You found a match!');
+} else {
+alert('Sorry, try again')
+	}
+
+};
